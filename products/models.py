c8114bcb3fa3a8933ce9244f2a0faf69f154aa61
eldrige <peldrige8@gmail.com>
2021-06-30 11:39:00 +0100

    done learning models/migrations

--- products/models.py
+++ products/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 
 # Create your models here.
+# blank = false, means the field is required, null refers to the val in the db
 
 
 class Product(models.Model):
-    title = models.TextField()
-    description = models.TextField()
-    price = models.TextField()
-    summary = models.TextField(default="This is cool!")
+    title = models.CharField(max_length=100)
+    description = models.TextField(blank=True, null=True)
+    price = models.DecimalField(decimal_places=2, max_digits=10000)
+    summary = models.TextField()
