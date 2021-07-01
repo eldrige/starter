92f2021cc44f6035754e9be201ac1f0994460cc3
eldrige <peldrige8@gmail.com>
2021-07-01 23:06:48 +0100

    start form modelling

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import response
 from .models import Product
+from .forms import ProductCreateForm
 
 # Create your views here.
 
@@ -12,3 +13,15 @@ def product_view(request, *args, **kwargs):
         'description': prod_one.description
     }
     return render(request, "product.html", context)
+
+
+def product_form_view(request):
+    form = ProductCreateForm(request.POST or None)
+
+    if form.is_valid():
+        form.save()
+    context = {
+        'form': form
+    }
+
+    return render(request, "form.html", context)
--- starter/urls.py
+++ starter/urls.py
@@ -17,12 +17,12 @@ from django.contrib import admin
 from django.urls import path
 
 from pages.views import home_view, about_view
-from products.views import product_view
+from products.views import product_view, product_form_view
 
 
 urlpatterns = [
     path('', home_view, name="home"),
-    path('about/', about_view, name="about"),
+    path('form/', product_form_view, name="form"),
     path('products/', product_view, name="product"),
     path('admin/', admin.site.urls),
 ]
