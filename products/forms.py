cdda46de6e8e1883754abdc5f6f2b9bf5d9fed76
eldrige <peldrige8@gmail.com>
2021-07-08 11:09:26 +0100

    form widgets

--- products/forms.py
+++ products/forms.py
@@ -11,3 +11,10 @@ class ProductCreateForm(forms.ModelForm):
             'description',
             'price'
         ]
+
+
+class RawProductForm(forms.Form):
+    title = forms.CharField()
+    description = forms.CharField(required=False, widget=forms.Textarea(
+        attrs={"class": "new-class-name two", "rows": 20, "cols": 100}))
+    price = forms.DecimalField()
--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import response
 from .models import Product
-from .forms import ProductCreateForm
+from .forms import ProductCreateForm, RawProductForm
 
 # Create your views here.
 
@@ -21,3 +21,12 @@ def product_form_view(request):
     print(my_new_title)
 
     return render(request, "form.html", {})
+
+
+def product_create_form(request):
+
+    my_form = RawProductForm(request.POST)
+    context = {
+        "form": my_form
+    }
+    return render(request, "product_create.html", context)
